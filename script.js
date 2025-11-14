151c80b651ac08d25fbb6bbfbcf441a54ef0196c
Weverton <wevertonmartinsdigital@gmail.com>
2025-11-14 08:53:24 -0300

    Otimização e contrução alternativa

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// Função para o menu mobile
 const btnMobile = document.querySelector('.btn-mobile');
 const navList = document.querySelector('.nav-list');
 
 btnMobile.addEventListener('click', () => {
+  const active = btnMobile.classList.toggle('active');
   navList.classList.toggle('active');
-  btnMobile.classList.toggle('active');
-  btnMobile.innerHTML = btnMobile.innerHTML === '☰' ? 'X' : '☰';
-  btnMobile.style.backgroundColor = btnMobile.innerHTML === 'X' ? '#FBEFE7' : '#EB7E00';
-  btnMobile.style.border = btnMobile.innerHTML === 'X' ? '1px solid #EB7E00' : '1px solid transparent';
-  btnMobile.style.color = btnMobile.innerHTML === 'X' ? '#EB7E00' : '#FFFFFF';
-})
+
+  btnMobile.textContent = active ? 'X' : '☰';
+});
